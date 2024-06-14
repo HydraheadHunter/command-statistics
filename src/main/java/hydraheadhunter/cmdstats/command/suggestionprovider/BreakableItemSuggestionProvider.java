f96cbbe9792c5a3144b2efb4f89ac696eacc7c09
AnDrew <repooc.a@gmail.com>
2024-06-14 07:32:34 -0500

    Replaced removed `IsBreakable()` method with the new `DataComponentType.DAMAGE` nonsense.

--- src/main/java/hydraheadhunter/cmdstats/command/suggestionprovider/BreakableItemSuggestionProvider.java
+++ src/main/java/hydraheadhunter/cmdstats/command/suggestionprovider/BreakableItemSuggestionProvider.java
@@ -8,19 +8,22 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import hydraheadhunter.cmdstats.CommandStatistics;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.suggestion.SuggestionProviders;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
 public class BreakableItemSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
      
-     public static final SuggestionProvider<ServerCommandSource> BREAKABLE_ITEMS = SuggestionProviders.register(new Identifier(CommandStatistics.MOD_ID, "breakable_items"),
+     public static final SuggestionProvider<ServerCommandSource> BREAKABLE_ITEMS = SuggestionProviders.register(Identifier.of(CommandStatistics.MOD_ID, "breakable_items"),
           (context, builder) -> CommandSource.suggestIdentifiers(
                Registries.ITEM.stream()
                     .filter(item -> item.isEnabled(context.getSource().getEnabledFeatures()) &&
-                         item.isDamageable())
+                         isDamageable(item))
                     .map(Registries.ITEM::getId),
                builder
           ));
@@ -28,4 +31,9 @@ public class BreakableItemSuggestionProvider implements SuggestionProvider<Serve
      public CompletableFuture<Suggestions> getSuggestions( CommandContext<ServerCommandSource> context, SuggestionsBuilder builder ) throws CommandSyntaxException {
           return null;
      }
+     private static boolean isDamageable(Item item) throws NullPointerException{
+          try { return item.getDefaultStack().getComponents().get(DataComponentTypes.DAMAGE)>0; }
+          catch (NullPointerException e) {return false;}
+     }
+     
 }
